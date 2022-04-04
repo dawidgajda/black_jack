c45a11c220e86aa5299321b9a7741ca1d6845489
Dawid Gajda <dawid.gajda3@gmail.com>
2022-04-04 23:24:24 +0200

    Add docstring in Deck

--- Deck.py
+++ Deck.py
@@ -1,25 +1,34 @@
+"""Module with class Deck
+
+    Returns:
+        list: shuffle deck
+    """
 from random import shuffle
 from Card import Card
 
 
-class Deck():
+class Deck:
+    """Class Deck with all colors and figure cards"""
+
     def __init__(self) -> None:
         self.colors = [{'CLUBS': '♣'}, {'DIAMONDS': '♦'},
                        {'HEARTS': '♥'}, {'SPADES': '♠'}]
-        self.figures = [{'A': 10}, {'K': 10}, {'Q': 10}, {'J': 10}, {'10': 10},
-                        {'9': 9}, {'8': 8}, {'7': 7}, {'6': 6}, {'5': 5}, {'4': 4}, {'3': 3}, {'2': 2}]
+        self.figures = [{'A': 10}, {'K': 10}, {'Q': 10}, {'J': 10},
+                        {'10': 10}, {'9': 9}, {'8': 8}, {'7': 7},
+                        {'6': 6}, {'5': 5}, {'4': 4}, {'3': 3}, {'2': 2}]
 
         self.deck = []
 
     def deck_generator(self):
+        """Method to generate deck with all card"""
         for color in self.colors:
             for figure in self.figures:
                 self.deck.append(Card(color, figure))
 
     def deck_shuffle(self):
+        """Method to shuffle deck
+
+        Returns:
+            list: card objects instance
+        """
         return shuffle(self.deck)
-
-
-deck = Deck()
-deck.deck_generator()
-deck.deck_shuffle()
