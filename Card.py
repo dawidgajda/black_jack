d01b1696d491c14c52158ec168d98331c2cf1c8a
Dawid Gajda <dawid.gajda3@gmail.com>
2022-04-04 21:58:19 +0200

    Add methods and testes  to class Card

--- Card.py
+++ Card.py
@@ -1,7 +1,24 @@
-class Card():
+class Card:
+
     def __init__(self, color, figure) -> None:
         self.color = color
         self.figure = figure
 
     def __repr__(self) -> str:
         return f'{self.color} {self.figure}'
+
+    def get_color_name(self):
+        color_name = list(self.color.keys())
+        return color_name[0]
+
+    def get_color_sign(self):
+        color_sign = list(self.color.values())
+        return color_sign[0]
+
+    def get_figure_name(self):
+        figure_name = list(self.figure.keys())
+        return figure_name[0]
+
+    def get_figure_values(self):
+        figure_value = list(self.figure.values())
+        return figure_value[0]
---
+++ card_test.py
@@ -0,0 +1,19 @@
+from Card import Card
+
+card = Card({'CLUBS': '♣'}, {'A': 10})
+
+
+def test_color_name():
+    assert card.get_color_name() == 'CLUBS'
+
+
+def test_color_sign():
+    assert card.get_color_sign() == '♣'
+
+
+def test_figure_sign():
+    assert card.get_figure_name() == 'A'
+
+
+def test_figure_value():
+    assert card.get_figure_values() == 10
