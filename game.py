3b2e0121c02e867daf01969c021c4a29a8ee0723
Dawid Gajda <dawid.gajda3@gmail.com>
2022-04-07 16:03:27 +0200

    Modifie class Game and Player

--- game.py
+++ game.py
@@ -5,16 +5,14 @@ from player import Player
 class Game():
     def __init__(self) -> None:
         self.deck = Deck()
-        self.player1 = Player()
-        self.player2 = Player()
+        self.player = Player(self.deck)
+        self.croupier = Player(self.deck)
 
     def start(self):
         self.deck.generator()
         self.deck.shuffle()
+        self.player.get_card_to_hand()
 
-    def get_card_to_hand(self):
-        card = self.deck.get_card()
-        card_repr = f'{card.get_figure_kind()} {card.get_color_sign()}'
-        print(f'Your card {card_repr}')
-        self.player1.add_card_point(card.get_figure_values())
-        return card_repr
+
+game = Game()
+game.start()
--- player.py
+++ player.py
@@ -4,8 +4,9 @@
 class Player:
     """Class Player"""
 
-    def __init__(self) -> None:
+    def __init__(self, deck) -> None:
         self.card_point = []
+        self.deck = deck
 
     def clear_point(self):
         """Function to clear list with player points"""
@@ -14,3 +15,25 @@ class Player:
     def add_card_point(self, card_value):
         """Function to add player points to list"""
         self.card_point.append(card_value)
+
+    def get_card_to_hand(self):
+        card = self.deck.get_card()
+        card_repr = f'{card.get_figure_kind()} {card.get_color_sign()}'
+        print(f'Your draw card {card_repr}')
+        self.add_card_point(card.get_figure_values())
+        print(f'Sum points {sum(self.card_point)}')
+        self.next_card_to_hand()
+
+    def next_card_to_hand(self):
+        next_card = input("Do you have next card? [Y]es or [N]o:")
+        # while next_card.upper() != 'N':
+        if next_card.upper() == 'Y':
+            self.get_card_to_hand()
+        elif next_card.upper() == 'N':
+            self.croupier()
+        else:
+            print('Wrong choice')
+            self.next_card_to_hand()
+
+    def croupier(self):
+        print('Croupier')
